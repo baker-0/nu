2194a1e10a16618b7ca1f5861b249051bdd7e358
Ahmed Baki <baki95@outlook.com>
2019-03-26 14:35:30 -0700

    Move imports to top of file

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ High level app routes
 
 require('dotenv').config()
 const express = require('express')
+const { loginController,
+  authController,
+  testController } = require('./controller.js')
+
 const app = express()
 
 app.use(express.static('public'))
-
-const { loginController, authController, testController } =
-    require('./controller.js')
 
 app.get('/login', loginController)
 
