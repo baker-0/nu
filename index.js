8c3a815130723a0d42013b0932a2d489fa523b59
Ahmed Baki <baki95@outlook.com>
2019-03-26 11:21:53 -0700

    Format index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.static('public'))
 
-const {loginController, authController, testController} =
+const { loginController, authController, testController } =
     require('./controller.js')
 
 app.get('/login', loginController)
@@ -14,5 +14,4 @@ app.get('/auth', authController)
 
 app.get('/test', testController)
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Nu server listening on port ${port}!`))
