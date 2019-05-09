4880ecbcb0a500ef96221a798d0e92b02c2f14ff
Ahmed Baki <baki95@outlook.com>
2019-05-08 23:38:39 -0700

    Add onClick function to landing page button

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -3,6 +3,11 @@ import './LandingPage.css';
 
 class LandingPage extends Component {
   handleSubmit = () => {
+    fetch('https://localhost:8888/login', {
+      mode: 'cors'
+    })
+      .then(res => console.log('res ', res))
+      .catch(err => console.error("Err: " + err))
 
   }
   render() {
@@ -12,12 +17,12 @@ class LandingPage extends Component {
         <div id='callToAction'>
           <p id="actionText" className='animated fadeInLeft'>You're <span className='highlight'>one</span> step away from
             <span role='img' aria-label='Fire' id='fire'>
-              <span id='fire1' className='animated lightSpeedIn'>🔥</span>
-              <span id='fire2' className='animated lightSpeedIn'>🔥</span>
-              <span id='fire3' className='animated lightSpeedIn'>🔥</span>
+              <span id='fire1' className='animated lightSpeedIn' role='img'>🔥</span>
+              <span id='fire2' className='animated lightSpeedIn' role='img'>🔥</span>
+              <span id='fire3' className='animated lightSpeedIn' role='img'>🔥</span>
             </span>
           </p>
-          <button id='spotifyButton' className='animated fadeInRight' type='button'>
+          <button type='button' id='spotifyButton' className='animated fadeInRight' onClick={this.handleSubmit}>
             Connect to Spotify
           <img src="./spotify.png" id="spotifyLogo"></img>
           </button>
