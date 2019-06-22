de7adb684f28874dd0a869ba60ec9bab4146dbad
Ahmed Baki <baki95@outlook.com>
2019-06-21 21:54:38 -0700

    Remove unused constructor, handler in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,9 @@ import { Switch, Route, Redirect } from 'react-router'
 import Loading from './components/Loading';
 
 const initialRedirect = () => {
-  if (localStorage.getItem('spotify-auth')) { // User already authenticated.
-    return <Redirect to="/dashboard" />
+  let spJWT = localStorage.getItem('spotify-auth')
+  if (spJWT !== 'undefined' && spJWT !== null) { // User already authenticated.
+    return <Redirect to="/authorized" />
   }
   else { // New user.
     return (<Redirect to="/login" />)
@@ -15,28 +16,13 @@ const initialRedirect = () => {
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    // Bind handler function to 'this' such that it can be called from child components.
-    this.authHandler = this.authHandler.bind(this)
-  }
-
-  authHandler(token) {
-    this.setState({
-      'auth_token': token
-    })
-  }
-  componentWillUpdate(nextProps, nextState) {
-    console.log('nextState :', nextState);
-  }
   render() {
     return (
       <div>
         <Switch>
           <Route exact path='/' render={initialRedirect} />
           <Route exact path='/login' component={LandingPage} />
-          <Route exact path='/authorized'
-            render={(props) => <Loading {...props} authHandler={this.authHandler} />} />
+          <Route exact path='/authorized' component={Loading} />
           <Route exact path='/dashboard' component={Dashboard} />
         </Switch>
       </div>
