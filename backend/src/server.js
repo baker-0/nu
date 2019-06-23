de307554260865493319205220e361a630ae48ae
Ahmed Baki <baki95@outlook.com>
2019-06-22 17:28:03 -0700

    Add documentation to auth routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,8 +12,11 @@ app.get('/user/top/tracks',
   jwt({ secret: process.env.JWT_SECRET }),
   serverAPI.topTracks)
 
+// Client browser should be directed to this endpoint to login using spotify
 app.get('/auth/spotify', serverAPI.auth)
 
+// Spotify redirects to this endpoint after grabbing user's credentials.
+// Credentials are saved and the user is redirected to the home page
 app.get('/auth/redirect', serverAPI.redirect, serverAPI.welcome)
 
 app.get('/*', function (req, res) {
